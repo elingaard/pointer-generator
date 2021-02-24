45da79702341c1a491158a50c0b66e511a6a0535
Martin Elingaard <martinelingaard@MacBook-Pro-tilhrende-Martin.local>
2021-02-24 10:44:41 +0100

    update docstrings and eval function

--- models.py
+++ models.py
@@ -1,114 +1,130 @@
 import tensorflow as tf
+from tensorflow import keras
 
-class Encoder(tf.keras.Model):
+class Encoder(keras.Model):
+    """Bi-directional GRU encoder"""
     def __init__(self, vocab_size, embedding_dim, hidden_units,embedding_matrix):
         super().__init__()
 
         self.hidden_units = hidden_units
         if embedding_matrix is not None:
-            self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim, weights=[embedding_matrix])
+            self.embedding = keras.layers.Embedding(vocab_size, embedding_dim, weights=[embedding_matrix])
         else:
-            self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
-        self.bi_gru = tf.keras.layers.Bidirectional(tf.keras.layers.GRU(
-            hidden_units,
-            return_sequences=True,
-            return_state=True,
-            recurrent_initializer='glorot_uniform',
-        ))
+            self.embedding = keras.layers.Embedding(vocab_size, embedding_dim)
+        self.bi_gru = keras.layers.Bidirectional(keras.layers.GRU(
+                hidden_units,
+                return_sequences=True,
+                return_state=True,
+                recurrent_initializer='glorot_uniform',
+            ))
 
-    def call(self, encoder_input,encoder_states):
-        # inputs: encoder_input = (batch_size, seq_length)
-        #         encoder_states = list[(batch_size, hidden_units),(batch_size, hidden_units)]
+    def call(self,encoder_input,encoder_states):
+        """Forward pass of encoder
+        Args:
+        encoder_input(int tensor: (batch_size,seq_length) ): sequence(s) of vocabulary ids
+        encoder_states(list, len=2): encoder forward and backward state
 
-        # embedding look-up layer
-        encoder_emb = self.embedding(encoder_input) # (batch_size,seq_length,embedding_dim)
+        Returns:
+        encoder_output(float tensor: (batch_size,seq_length,hidden_dim) ): encoded space of each sequence
+        encoder_states(list, len=2): updated encoder states
+        """
 
-        # encoder_output = (batch_size,seq_length,hidden_units)
-        # encoder_states = (batch_size,hidden_units)
+        encoder_emb = self.embedding(encoder_input)
         encoder_output, state_fwd, state_back = self.bi_gru(encoder_emb,initial_state=encoder_states)
         encoder_states = [state_fwd,state_back]
 
         return encoder_output, encoder_states
 
-class BahdanauAttention(tf.keras.Model):
+class BahdanauAttention(keras.Model):
+    """Attention layer as described in: Neural Machine Translation by Jointly Learning to Align and Translate"""
     def __init__(self, hidden_units,is_coverage=False):
         super().__init__()
 
-        self.Wh = tf.keras.layers.Dense(hidden_units) # weight matrix for encoder hidden state
-        self.Ws = tf.keras.layers.Dense(hidden_units) # weight matrix for decoder state
-        self.V = tf.keras.layers.Dense(1)
+        self.Wh = keras.layers.Dense(hidden_units) # weight matrix for encoder hidden state
+        self.Ws = keras.layers.Dense(hidden_units) # weight matrix for decoder state
+        self.V = keras.layers.Dense(1)
         self.coverage = is_coverage
         if self.coverage is False:
-            self.wc = tf.keras.layers.Dense(1,kernel_initializer='zeros') # weight vector for coverage
+            self.wc = keras.layers.Dense(1,kernel_initializer='zeros') # weight vector for coverage
             self.wc.trainable = False
         else:
-            self.wc = tf.keras.layers.Dense(1)
+            self.wc = keras.layers.Dense(1)
 
     def call(self, decoder_state, encoder_output,coverage_vector):
-        # inputs: decoder_state = (batch_size, hidden_units)
-        #         encoder_output = (batch_size, seq_length, hidden_units)
-        #         coverage_vector = (batch_size, seq_length)
+        """Forward pass of attention layer
+        Args:
+        decoder_state(float tensor: (batch_size,hidden_dim) )
+        encoder_output(float tensor: (batch_size,seq_length,hidden_dim) )
+        coverage_vector(float tensor: (batch_size,seq_length) )
 
-        # expand dimension of decoder state and coverage vector to allow addition
-        decoder_state = tf.expand_dims(decoder_state, 1) # (batch_size, 1, hidden_units)
-        coverage_vector = tf.expand_dims(coverage_vector, 1) # (batch_size, 1, seq_length)
+        Returns:
+        context_vector(float tensor: (batch_size,hidden_dim) )
+        attention_weights(float tensor: (batch_size,seq_length) )
+        coverage_vector(float tensor: (batch_size,seq_length) )
+        """
 
         # calculate attention scores
-        # score = (batch_size, length, 1)
+        decoder_state = tf.expand_dims(decoder_state, 1)
+        coverage_vector = tf.expand_dims(coverage_vector, 1)
         score = self.V(tf.nn.tanh(
-                        self.Wh(encoder_output) +  # (batch_size, length, hidden_units) -> (batch_size, length, attention_units)
-                        self.Ws(decoder_state) +  # (batch_size, 1, hidden_units) -> (batch_size, 1, attention_units)
-                        self.wc(coverage_vector) # (batch_size, 1, seq_length) -> (batch_size, 1, 1)
+                        self.Wh(encoder_output) +
+                        self.Ws(decoder_state) +
+                        self.wc(coverage_vector)
                         ))
 
-        attention_weights = tf.nn.softmax(score, axis=1) # (batch_size, seq_length, 1)
-        # only update coverage vector if coverage is enabled
-        coverage_vector = tf.squeeze(coverage_vector,1) # (batch_size, seq_length)
+        attention_weights = tf.nn.softmax(score, axis=1)
+        coverage_vector = tf.squeeze(coverage_vector,1)
         if self.coverage is True:
           coverage_vector+=tf.squeeze(attention_weights)
-
-        context_vector = attention_weights * encoder_output # (batch_size, seq_length, hidden_units)
-        context_vector = tf.reduce_sum(context_vector, axis=1) # (batch_size, hidden_units)
+        context_vector = attention_weights * encoder_output
+        context_vector = tf.reduce_sum(context_vector, axis=1)
 
         return context_vector, attention_weights, coverage_vector
 
-class Decoder(tf.keras.Model):
+class Decoder(keras.Model):
+    """Bi-directional GRU decoder with two dense layers in the end to model the vocabulary distribution"""
     def __init__(self, vocab_size, embedding_dim, hidden_units,embedding_matrix):
         super().__init__()
 
         self.hidden_units = hidden_units
         if embedding_matrix is not None:
-            self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim, weights=[embedding_matrix])
+            self.embedding = keras.layers.Embedding(vocab_size, embedding_dim, weights=[embedding_matrix])
         else:
-            self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
-        self.gru = tf.keras.layers.GRU(
+            self.embedding = keras.layers.Embedding(vocab_size, embedding_dim)
+        self.gru = keras.layers.GRU(
             hidden_units,
             return_sequences=True,
             return_state=True,
             recurrent_initializer='glorot_uniform',
         )
-        self.W1 = tf.keras.layers.Dense(hidden_units)
-        self.W2 = tf.keras.layers.Dense(vocab_size)
+        self.W1 = keras.layers.Dense(hidden_units)
+        self.W2 = keras.layers.Dense(vocab_size)
 
 
     def call(self, decoder_input, decoder_state, encoder_output,context_vector):
-        # inputs: decoder_input = (batch_size, 1)
-        #         decoder_state = (batch_size, hidden_units)
-        #         encoder_output = (batch_size,seq_length, hidden_units)
-        #         coverage_vector = (batch_size,seq_length)
+        """Forward pass of decoder
 
-        # embedding look-up layer
+        Args:
+        decoder_input(int tensor: (batch_size,1) )
+        decoder_state(float tensor: (batch_size,hidden_dim) )
+        encoder_output(float tensor: (batch_size,seq_length,hidden_dim) )
+        coverage_vector(float tensor: (batch_size,seq_length))
+
+        Returns:
+        p_vocab(float tensor: (batch_size,vocab_size) )
+        decoder_state(float tensor: (batch_size,hidden_dim) )
+        """
+
         decoder_emb = self.embedding(decoder_input) # (batch_size, seq_length, hidden_units)
-
-        # decoder_output = (batch_size,seq_length,hidden_units)
-        # decoder_state = (batch_size,hidden_units)
         decoder_output , decoder_state = self.gru(decoder_emb,initial_state=decoder_state)
-
-        # concatenate context vector and decoder state
         concat_vector = tf.concat([context_vector,decoder_state], axis=-1)
-        # reshape to 1d array
         concat_vector = tf.reshape(concat_vector, (-1, concat_vector.shape[1]))
-        # create vocabulary distribution
         p_vocab = tf.nn.log_softmax(self.W2(self.W1(concat_vector)))
 
         return p_vocab, decoder_state
+
+
+
+
+
+
--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow import keras
 
 class Vocab(object):
+    """Class for storing the mapping between words and their corresponding index in the vocabulary"""
     def __init__(self,tokenizer,max_size):
         self._word_to_id = {}
         self._id_to_word = {}
@@ -15,7 +16,6 @@ class Vocab(object):
             self._count += 1
             if self._count >= max_size:
                 break
-        print("Finished constructing vocabulary of %i total words. Last word added: %s" % (self._count, self._id_to_word[self._count-1]))
 
     def word2id(self, word):
         """Returns the id (integer) of a word (string). Returns [UNK] id if word is OOV."""
@@ -29,44 +29,50 @@ class Vocab(object):
             raise ValueError('Id not found in vocab: %d' % word_id)
         return self._id_to_word[word_id]
 
+    def decode_seq(self,seq):
+        return " ".join([self._id_to_word[idx] for idx in seq])
+
     def size(self):
         """Returns the total size of the vocabulary"""
         return self._count
 
 def text2seq(text,tokenizer,vocab):
+    """Convert a string or list of strings to a sequence of vocabulary ids
+
+    Args:
+    text(string or list of strings): text input
+    tokenizer(object): tokenizer object
+    vocab(object): vocabulary object
+
+    Returns:
+    seqs_padded(int tensor): sequence of vocabulary ids padded with start and end token ids
+    """
     seqs = tokenizer.texts_to_sequences(text)
     seqs = [[vocab._word_to_id['<s>']]+seq+[vocab._word_to_id['<\s>']] for seq in seqs]
     max_len_seq = max([len(s) for s in seqs])
     seqs_padded = keras.preprocessing.sequence.pad_sequences(seqs, maxlen=max_len_seq, padding="post")
     return seqs_padded
 
-def decode_seq(seq):
-    return " ".join([vocab._id_to_word[idx] for idx in seq])
-
-def greedy_search(encoder_input,max_len_sum = 30):
+def greedy_search(encoder_input,model,vocab,max_len_sum = 30):
     """Function which returns a summary by always picking the highest probability option conditioned on the previous word"""
-    # run body_sequence through encoder
-    encoder_init_states = [tf.zeros((1, encoder.hidden_units)) for i in range(2)]
-    encoder_output, encoder_states = encoder(encoder_input,encoder_init_states)
-    # initialize decoder with encoder forward state
+    encoder_init_states = [tf.zeros((1, model.encoder.hidden_units)) for i in range(2)]
+    encoder_output, encoder_states = model.encoder(encoder_input,encoder_init_states)
     decoder_state = encoder_states[0]
 
-    decoder_input_t = tf.ones(1)*vocab._word_to_id['<s>'] # initialize with start token
+    decoder_input_t = tf.ones(1)*vocab._word_to_id['<s>']
     summary = [vocab._word_to_id['<s>']]
     coverage_vector = tf.zeros((1,encoder_input.shape[1]))
-    while decoder_input_t[0].numpy()!=vocab._word_to_id['<\s>'] and len(summary)<max_len_sum: # as long as decoder input is different from end token continue
-        context_vector, attention_weights, coverage_vector = attention(decoder_state, encoder_output,coverage_vector)
-        p_vocab, decoder_state = decoder(tf.expand_dims(decoder_input_t,1),decoder_state,encoder_output,context_vector)
+    while decoder_input_t[0].numpy()!=vocab._word_to_id['<\s>'] and len(summary)<max_len_sum:
+        context_vector, attention_weights, coverage_vector = model.attention_model(decoder_state, encoder_output,coverage_vector)
+        p_vocab, decoder_state = model.decoder(tf.expand_dims(decoder_input_t,1),decoder_state,encoder_output,context_vector)
         decoder_input_t = tf.argmax(p_vocab,axis=1)
         decoder_word_idx = int(decoder_input_t[0].numpy())
         summary.append(decoder_word_idx)
     return summary
 
-def beam_search(encoder_input,beam_size=4,n_keep=4,max_len_sum=30):
-    # run body_sequence through encoder
-    encoder_init_states = [tf.zeros((1, encoder.hidden_units)) for i in range(2)]
-    encoder_output, encoder_states = encoder(encoder_input,encoder_init_states)
-    # initialize decoder with encoder forward state
+def beam_search(encoder_input,model,vocab,beam_size=4,n_keep=4,max_len_sum=30):
+    encoder_init_states = [tf.zeros((1, model.encoder.hidden_units)) for i in range(2)]
+    encoder_output, encoder_states = model.encoder(encoder_input,encoder_init_states)
     decoder_state = encoder_states[0]
 
     coverage_vector = tf.zeros((1,encoder_input.shape[1]))
@@ -79,8 +85,8 @@ def beam_search(encoder_input,beam_size=4,n_keep=4,max_len_sum=30):
             if cand[1][-1]!=vocab._word_to_id['<\s>']:
                 decoder_input_t = tf.ones(1)*cand[1][-1]
                 decoder_state, coverage_vector = cand[2]
-                context_vector, attention_weights, coverage_vector = attention(decoder_state, encoder_output,coverage_vector)
-                p_vocab, decoder_state = decoder(tf.expand_dims(decoder_input_t,1),decoder_state,encoder_output,context_vector)
+                context_vector, attention_weights, coverage_vector = model.attention_model(decoder_state, encoder_output,coverage_vector)
+                p_vocab, decoder_state = model.decoder(tf.expand_dims(decoder_input_t,1),decoder_state,encoder_output,context_vector)
                 values,indicies = tf.math.top_k(p_vocab,k=beam_size)
                 for val,idx in zip(values.numpy()[0],indicies.numpy()[0]):
                     new_idx_list = cand[1] + [idx]
@@ -95,7 +101,7 @@ def beam_search(encoder_input,beam_size=4,n_keep=4,max_len_sum=30):
     return candidates
 
 def masked_nll_loss(p_vocab,target):
-    # apply a mask such that pad zeros do not affect the loss
+    """Calculate negative log-likelihood loss and use mask to ignore padding"""
     mask = tf.math.logical_not(tf.math.equal(target, 0))
     loss = -p_vocab
     mask = tf.cast(mask, dtype=loss.dtype)
